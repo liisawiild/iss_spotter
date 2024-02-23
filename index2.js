faa74403cd383f34ffe48d5bba86e26a68ee81f2
Lisa Wild <lisakwild@gmail.com>
2024-02-22 23:41:27 -0800

    add fetchISSFlyoverTimes to promise chain

--- index2.js
+++ index2.js
@@ -1,4 +1,6 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss_promised');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes } = require('./iss_promised');
 
 fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyoverTimes)
   .then((body) => console.log(body));
