8d7889e14a58b4c35b4ab7a52a56fe08bbabd5f9
Lisa Wild <lisakwild@gmail.com>
2024-02-23 01:20:45 -0800

    add index2.js promises to the nextISSTimesForMyLocation, and return the pass times

--- iss_promised.js
+++ iss_promised.js
@@ -4,14 +4,24 @@ const fetchMyIP = function() {
   return request('https://api.ipify.org/?format=json');
 };
 
-const fetchCoordsByIP = function (body) {
+const fetchCoordsByIP = function(body) {
   let ipAddress = JSON.parse(body).ip;
   return request(`https://ipwho.is/${ipAddress}`);
 };
 
 const fetchISSFlyoverTimes = function(body) {
-  let coords = JSON.parse(body);
-  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`);
+  const coords = JSON.parse(body);
+  return request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`);
+};
+
+const nextISSTimesForMyLocation = function(body) {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyoverTimes)
+  .then((body) => {
+    const times = JSON.parse(body).response;
+    return times;
+  })
 }
 
-module.exports = { fetchMyIP, fetchCoordsByIP, fetchISSFlyoverTimes  };
+module.exports = { nextISSTimesForMyLocation };
