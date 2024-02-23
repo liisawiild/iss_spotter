15062e21f6ee0c384c42f7f3dc2982923b140a9e
Lisa Wild <lisakwild@gmail.com>
2024-02-22 18:33:54 -0800

    add coords from fetchCoordsByIP as fetchISSFlyeroverTimes first argument

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const {  fetchISSFlyoverTimes } = require('./iss');
+// fetchMyIP, fetchCoordsByIP,
 
-fetchMyIP((error, ip) => {
-  if (error) {
-    console.log("It didn't work!" , error);
-    return;
-  }
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
 
-  console.log('It worked! Returned IP:' , ip);
-});
+//   console.log('It worked! Returned IP:' , ip);
+// });
 
-fetchCoordsByIP('64.180.44.26', (error, data) => {
+// fetchCoordsByIP('64.180.44.26', (error, data) => {
+//   if (error) {
+//     console.log("It didn't work! Error: ", error);
+
+//   }
+//   console.log(data);
+// });
+
+fetchISSFlyoverTimes({latitude: 49.2837626, longitude: -122.7932065}, (error, data) => {    
   if (error) {
     console.log("It didn't work! Error: ", error);
 
   }
   console.log(data);
 });
+
+
