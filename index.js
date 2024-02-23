32c6ad10151e0bd987493706de107343d9d4db85
Lisa Wild <lisakwild@gmail.com>
2024-02-22 18:58:18 -0800

    lint code

--- index.js
+++ index.js
@@ -13,15 +13,15 @@ const {  fetchISSFlyoverTimes } = require('./iss');
 // fetchCoordsByIP('64.180.44.26', (error, data) => {
 //   if (error) {
 //     console.log("It didn't work! Error: ", error);
-
+//     return;
 //   }
 //   console.log(data);
 // });
 
-fetchISSFlyoverTimes({latitude: 49.2837626, longitude: -122.7932065}, (error, data) => {    
+fetchISSFlyoverTimes({latitude: 49.2837626, longitude: -122.7932065}, (error, data) => {
   if (error) {
     console.log("It didn't work! Error: ", error);
-
+    return;
   }
   console.log(data);
 });
